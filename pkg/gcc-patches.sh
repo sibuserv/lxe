ee009dc17d6d8d6e7fc253083dea00f49d64d013
Boris Pek <tehnick-8@yandex.ru>
2015-06-17 13:20:43 +0300

    gcc: add patch for gcc-5.1.0 (fix libsanitizer build without RPC)

--- pkg/gcc-patches.sh
+++ pkg/gcc-patches.sh
@@ -5,6 +5,13 @@
         SUBDIR="${PKG_SUBDIR_ORIG}" || \
         SUBDIR="${PKG_SUBDIR}"
 
+    if [ "${GCC_VER}" = "5.1.0" ]
+    then
+        LOG_FILE="${LOG_DIR}/${PKG_SUBDIR}/patch.log"
+        cd "${PKG_SRC_DIR}/${SUBDIR}"
+        patch -p1 -i "${PKG_DIR}/gcc-5.1.0.patch" &> "${LOG_FILE}"
+    fi
+
     if [ "$(ls "${PKG_SRC_DIR}/${SUBDIR}"/gmp-*.tar.* 2>/dev/null | wc -l)" = "0" ]
     then
         LOG_FILE="${LOG_DIR}/${PKG_SUBDIR}/bootstrap.log"
