0abf825c194252620d0d55812702cc8d0f1dd389
Boris Pek <tehnick-8@yandex.ru>
2016-07-05 23:01:37 +0300

    giflib: fix PKG_URL for giflib >= 5.0.6

--- pkg/giflib.sh
+++ pkg/giflib.sh
@@ -7,9 +7,18 @@
     PKG_VERSION=${GIFLIB_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.bz2
-    IsPkgVersionGreaterOrEqualTo "5.0.0" && \
-        PKG_URL="http://sourceforge.net/projects/${PKG}/files/giflib-5.x/${PKG_FILE}" || \
+    if IsPkgVersionGreaterOrEqualTo "5.0.6"
+    then
+        PKG_URL="http://sourceforge.net/projects/${PKG}/files/${PKG_FILE}"
+    elif IsPkgVersionGreaterOrEqualTo "5.0.0"
+    then
+        PKG_URL="http://sourceforge.net/projects/${PKG}/files/giflib-5.x/${PKG_FILE}"
+    elif IsPkgVersionGreaterOrEqualTo "4.2.0"
+    then
+        PKG_URL="http://sourceforge.net/projects/${PKG}/files/giflib-4.x/${PKG_FILE}"
+    else
         PKG_URL="http://sourceforge.net/projects/${PKG}/files/giflib-4.x/${PKG_SUBDIR}/${PKG_FILE}"
+    fi
     PKG_DEPS="gcc zlib jpeg"
 
     if ! IsPkgInstalled
