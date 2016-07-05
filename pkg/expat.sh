b3354d5fa3eaa75561f5f5cc91687505f83030c4
Boris Pek <tehnick-8@yandex.ru>
2016-07-05 22:55:54 +0300

    expat: fix PKG_FILE for expat >= 2.1.1

--- pkg/expat.sh
+++ pkg/expat.sh
@@ -6,7 +6,9 @@
     PKG=expat
     PKG_VERSION=${EXPAT_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
-    PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
+    IsPkgVersionGreaterOrEqualTo "2.1.1" && \
+        PKG_FILE=${PKG}-${PKG_VERSION}.tar.bz2 || \
+        PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
     PKG_URL="http://sourceforge.net/projects/${PKG}/files/${PKG}/${PKG_VERSION}/${PKG_FILE}"
     PKG_DEPS="gcc"
 
