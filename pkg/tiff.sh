1209c48474420fb59e03c177808958e8dd68b398
Boris Pek <tehnick-8@yandex.ru>
2016-07-06 01:37:47 +0300

    tiff: fix PKG_URL for tiff < 3.9.4

--- pkg/tiff.sh
+++ pkg/tiff.sh
@@ -7,7 +7,9 @@
     PKG_VERSION=${TIFF_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
-    PKG_URL="http://download.osgeo.org/libtiff/${PKG_FILE}"
+    IsPkgVersionGreaterOrEqualTo "3.9.4" && \
+        PKG_URL="http://download.osgeo.org/libtiff/${PKG_FILE}" || \
+        PKG_URL="http://download.osgeo.org/libtiff/old/${PKG_FILE}"
     PKG_DEPS="gcc"
 
     if ! IsPkgInstalled
