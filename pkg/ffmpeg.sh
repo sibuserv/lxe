8e2babe78cf5830f10559ad6164efff5d1900a86
Boris Pek <tehnick-8@yandex.ru>
2016-07-08 00:58:43 +0300

    Add packages x264 and ffmpeg.

---
+++ pkg/ffmpeg.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+[ -z "${FFMPEG_VER}" ] && exit 1
+
+(
+    PKG=ffmpeg
+    PKG_VERSION=${FFMPEG_VER}
+    PKG_SUBDIR=${PKG}-${PKG_VERSION}
+    PKG_FILE=${PKG_SUBDIR}.tar.bz2
+    PKG_URL="http://www.ffmpeg.org/releases/${PKG_FILE}"
+    PKG_DEPS="gcc bzip2 yasm zlib"
+
+    if ! IsPkgInstalled
+    then
+        CheckDependencies
+
+        GetSources
+        UnpackSources
+        PrepareBuild
+
+        SetBuildFlags
+        SetCrossToolchainPath
+        SetCrossToolchainVariables
+        LDFLAGS="${LDFLAGS} -lrt"
+        ConfigurePkg \
+            --prefix="${SYSROOT}/usr" \
+            --sysroot="${SYSROOT}" \
+            ${LIB_TYPE_OPTS} \
+            --arch="${ARCH}" \
+            --target-os=linux \
+            --cross-prefix="${TARGET}-" \
+            --enable-cross-compile \
+            --enable-avisynth \
+            --enable-avresample \
+            --disable-debug \
+            --disable-doc \
+            --disable-libx264
+
+        BuildPkg -j ${JOBS}
+        InstallPkg install
+
+        CleanPkgBuildDir
+        CleanPkgSrcDir
+    fi
+)
+
---
+++ pkg/x264.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+[ -z "${X264_VER}" ] && exit 1
+
+(
+    PKG=x264
+    PKG_VERSION=${X264_VER}
+    PKG_SUBDIR=${PKG}-${PKG_VERSION}
+    PKG_SUBDIR_ORIG=${PKG}-snapshot-${PKG_VERSION}
+    PKG_FILE=${PKG}-snapshot-${PKG_VERSION}.tar.bz2
+    PKG_URL="http://download.videolan.org/pub/videolan/${PKG}/snapshots/${PKG_FILE}"
+    PKG_DEPS="gcc"
+
+    if ! IsPkgInstalled
+    then
+        ProcessStandardAutotoolsProject \
+            --cross-prefix="${TARGET}-" \
+            --disable-asm \
+            --disable-lavf \
+            --disable-swscale
+    fi
+)
+
