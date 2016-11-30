55da3934232c0bedb4aca4a807e988dc0b74fa0b
Boris Pek <tehnick-8@yandex.ru>
2016-12-01 01:54:06 +0300

    Update X264_VER from 20160706-2245 to 20161130-2245 in all configs.

--- pkg/exiv2.sh
+++ pkg/exiv2.sh
@@ -7,7 +7,7 @@
     PKG_VERSION=${EXIV2_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG_SUBDIR}.tar.gz
-    PKG_URL="http://www.${PKG}.org/${PKG_FILE}"
+    PKG_URL="http://www.exiv2.org/${PKG_FILE}"
     PKG_DEPS="gcc zlib expat"
 
     if ! IsPkgInstalled
