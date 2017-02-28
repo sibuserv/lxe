9dd324cdfb45879137af6a56ca3d0792acc05ee9
Boris Pek <tehnick-8@yandex.ru>
2017-03-01 02:09:19 +0300

    Add new package: qtdatavis3d.

---
+++ pkg/qtdatavis3d.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+[ -z "${QT5_VER}" ] && exit 1
+
+(
+    PKG=qtdatavis3d
+    PKG_DEPS="qtbase qtdeclarative qtmultimedia"
+
+    if IsVer1GreaterOrEqualToVer2 "${QT5_VER}" "5.7.0"
+    then
+        . "${PKG_DIR}/qtmodule.sh"
+    else
+        CheckDependencies
+
+        date -R > "${INST_DIR}/${PKG}"
+        echo "[config]   ${CONFIG}"
+        echo "[no-build] ${PKG}"
+    fi
+)
+
