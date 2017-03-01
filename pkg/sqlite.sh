6f00e44153c5b22ceef9f0a48a3feb0547cdb7ae
Boris Pek <tehnick-8@yandex.ru>
2017-03-01 23:46:16 +0300

    Add new package: sqlite.

---
+++ pkg/sqlite.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+[ -z "${SQLITE_VER}" ] && exit 1
+
+(
+    PKG=sqlite
+    PKG_VERSION=${SQLITE_VER}
+    PKG_SUBDIR=${PKG}-${PKG_VERSION}
+    PKG_SUBDIR_ORIG=SQLite-${SQLITE_FOSSIL_VER}
+    PKG_FILE=SQLite-${SQLITE_FOSSIL_VER}.tar.gz
+    # http://www.sqlite.org/cgi/src/taglist
+    PKG_URL="http://www.sqlite.org/cgi/src/tarball/${PKG_FILE}"
+    PKG_DEPS="gcc"
+
+    if ! IsPkgInstalled
+    then
+        ProcessStandardAutotoolsProject \
+            --disable-readline \
+            --enable-threadsafe
+    fi
+)
+
