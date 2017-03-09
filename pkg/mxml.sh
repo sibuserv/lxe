576fcbb3d7636f42e9dbce9c63e370ee464b97fb
Boris Pek <tehnick-8@yandex.ru>
2017-03-09 23:16:19 +0300

    Add new package: mxml.

---
+++ pkg/mxml.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+[ -z "${MXML_VER}" ] && exit 1
+
+(
+    PKG=mxml
+    PKG_VERSION=${MXML_VER}
+    PKG_SUBDIR=${PKG}-${PKG_VERSION}
+    PKG_FILE=${PKG_SUBDIR}.tar.gz
+    PKG_URL="https://github.com/michaelrsweet/mxml/releases/download/release-${PKG_VERSION}/${PKG_FILE}"
+    PKG_DEPS="gcc"
+    [ ! -z "${GCC_EXTRA_VER}" ] && PKG_DEPS="${PKG_DEPS} gcc-extra"
+
+    if ! IsPkgInstalled
+    then
+        CheckDependencies
+
+        GetSources
+        UnpackSources
+        CopySrcAndPrepareBuild
+
+        SetBuildFlags "${GCC_EXTRA_VER}"
+        UpdateGCCSymlinks "${GCC_EXTRA_VER}"
+        SetCrossToolchainVariables "${GCC_EXTRA_VER}"
+        SetCrossToolchainPath
+        PrepareLibTypeOpts "static"
+        ConfigureAutotoolsProjectInBuildDir \
+            --enable-threads
+
+        BuildPkg -j ${JOBS}
+        InstallPkg install
+
+        CleanPkgBuildDir
+        CleanPkgSrcDir
+
+        UpdateGCCSymlinks
+    fi
+)
+
