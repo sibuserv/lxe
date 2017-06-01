26fd01cfdf2cf5a09f37cf84025367ad0141ee3a
Boris Pek <tehnick-8@yandex.ru>
2017-06-02 00:19:32 +0300

    libpng: update PKG_URL

--- pkg/libpng.sh
+++ pkg/libpng.sh
@@ -7,9 +7,9 @@
     PKG_VERSION=${LIBPNG_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.xz
-    IsPkgVersionGreaterOrEqualTo "1.2.55" && \
-        PKG_URL="http://sourceforge.net/projects/${PKG}/files/libpng12/${PKG_VERSION}/${PKG_FILE}" || \
-        PKG_URL="http://sourceforge.net/projects/${PKG}/files/libpng12/older-releases/${PKG_VERSION}/${PKG_FILE}"
+    IsPkgVersionGreaterOrEqualTo "1.2.56" && \
+        PKG_URL="https://sourceforge.net/projects/${PKG}/files/libpng12/${PKG_VERSION}/${PKG_FILE}" || \
+        PKG_URL="https://sourceforge.net/projects/${PKG}/files/libpng12/older-releases/${PKG_VERSION}/${PKG_FILE}"
     PKG_DEPS="gcc zlib"
 
     if ! IsPkgInstalled
