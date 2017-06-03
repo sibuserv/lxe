c651249b556980a78f693383ca33cb9dca511ce2
Boris Pek <tehnick-8@yandex.ru>
2017-06-03 10:24:22 +0300

    tiff: update PKG_URL detection for tiff < 4.0.4

--- pkg/tiff.sh
+++ pkg/tiff.sh
@@ -7,7 +7,7 @@
     PKG_VERSION=${TIFF_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
-    IsPkgVersionGreaterOrEqualTo "3.9.4" && \
+    IsPkgVersionGreaterOrEqualTo "4.0.4" && \
         PKG_URL="http://download.osgeo.org/libtiff/${PKG_FILE}" || \
         PKG_URL="http://download.osgeo.org/libtiff/old/${PKG_FILE}"
     PKG_DEPS="gcc"
