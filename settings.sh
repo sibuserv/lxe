93698037adc3a0b66cffec48dafb516ca2d06143
Boris Pek <tehnick-8@yandex.ru>
2017-07-13 04:18:28 +0300

    Update settings.sh for "hobby" branch.

--- settings.sh
+++ settings.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 
 # CONFIGS="Ubuntu-14.04-LTS_i386_static Ubuntu-14.04-LTS_amd64_static"
-# CONFIGS="Ubuntu-14.04-LTS_i386_shared Ubuntu-14.04-LTS_amd64_shared"
+CONFIGS="Ubuntu-14.04-LTS_i386_shared Ubuntu-14.04-LTS_amd64_shared"
 # CONFIGS="AstraLinux-1.2_static AstraLinux-1.3_static AstraLinux-1.4_static"
 # CONFIGS="AstraLinux-1.2_shared AstraLinux-1.3_shared AstraLinux-1.4_shared"
 
-CONFIGS="
-        AstraLinux-1.4_static
-        AstraLinux-1.4_shared
-        AstraLinux-1.3_static
-        AstraLinux-1.3_shared
-        AstraLinux-1.2_static
-        AstraLinux-1.2_shared
-        Ubuntu-14.04-LTS_i386_static
-        Ubuntu-14.04-LTS_i386_shared
-        Ubuntu-14.04-LTS_amd64_static
-        Ubuntu-14.04-LTS_amd64_shared
-        "
+# CONFIGS="
+#         AstraLinux-1.4_static
+#         AstraLinux-1.4_shared
+#         AstraLinux-1.3_static
+#         AstraLinux-1.3_shared
+#         AstraLinux-1.2_static
+#         AstraLinux-1.2_shared
+#         Ubuntu-14.04-LTS_i386_static
+#         Ubuntu-14.04-LTS_i386_shared
+#         Ubuntu-14.04-LTS_amd64_static
+#         Ubuntu-14.04-LTS_amd64_shared
+#         "
 
 # Type of current system:
 BUILD="$(LC_ALL=C gcc -v 2>&1 | sed -ne "s|^Target: \(.*\)$|\1|p")"
 # BUILD=x86_64-linux-gnu
 
 # Number of compilation processes during building of each package:
-JOBS=$(nproc 2>/dev/null || echo 1)
-# JOBS=4
+# JOBS=$(nproc 2>/dev/null || echo 1)
+JOBS=7
 
 # Default list of packages:
 # LOCAL_PKG_LIST="gcc pkg-config-settings cmake-settings ldd-settings"
-LOCAL_PKG_LIST="cmake-settings ldd-settings freeglut sdl2 qt5 qwt protobuf libjpeg-turbo boost ffmpeg openscenegraph"
+LOCAL_PKG_LIST="cmake-settings ldd-settings boost aspell hunspell minizip libotr tidy-html5 qtbase qttools qtmultimedia qtwebkit qca"
 
 # Delete unpacked source files after successful build of the package (true/false):
 CLEAN_SRC_DIR=true
