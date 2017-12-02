8d12e7a89a1b5870724599c87d4683c111cdec10
Boris Pek <tehnick-8@yandex.ru>
2017-12-03 00:34:11 +0300

    Package freetype (optionally) depends on package harfbuzz now.

--- pkg/freetype.sh
+++ pkg/freetype.sh
@@ -9,12 +9,16 @@
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.bz2
     PKG_URL="http://sourceforge.net/projects/${PKG}/files/freetype2/${PKG_VERSION}/${PKG_FILE}"
     PKG_DEPS="gcc pkg-config-settings zlib bzip2 libpng"
+    [ ! -z "${HARFBUZZ_VER}" ] && PKG_DEPS="${PKG_DEPS} harfbuzz"
 
     if ! IsPkgInstalled
     then
+        [ -z "${HARFBUZZ_VER}" ] && \
+            EXTRA_CONFIGURE_OPTS="${EXTRA_CONFIGURE_OPTS}
+                                  --without-harfbuzz"
         ProcessStandardAutotoolsProjectInBuildDir \
             --with-gnu-ld \
-            --without-harfbuzz
+            ${EXTRA_CONFIGURE_OPTS}
 
         cd "${PREFIX}/bin/"
         ln -sf "${SYSROOT}/usr/bin/${PKG}-config" "${TARGET}-${PKG}-config"
