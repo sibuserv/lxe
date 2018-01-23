14e1a003924d07b232bcf4c79c4f6926df740d45
Boris Pek <tehnick-8@yandex.ru>
2018-01-24 01:41:35 +0300

    Update settings.sh.

--- settings.sh
+++ settings.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-# CONFIGS="Ubuntu-14.04-LTS_i386_static Ubuntu-14.04-LTS_amd64_static"
-# CONFIGS="Ubuntu-14.04-LTS_i386_shared Ubuntu-14.04-LTS_amd64_shared"
-# CONFIGS="AstraLinux-1.2_static AstraLinux-1.3_static AstraLinux-1.4_static"
-# CONFIGS="AstraLinux-1.2_shared AstraLinux-1.3_shared AstraLinux-1.4_shared"
-
+# All available configs are in "etc/" subdirectory. Select necessary of them:
 CONFIGS="
         AstraLinux-1.4_static
         AstraLinux-1.4_shared
         AstraLinux-1.3_static
         AstraLinux-1.3_shared
         AstraLinux-1.2_static
-        AstraLinux-1.2_shared
-        Ubuntu-14.04-LTS_i386_static
-        Ubuntu-14.04-LTS_i386_shared
         Ubuntu-14.04-LTS_amd64_static
         Ubuntu-14.04-LTS_amd64_shared
+        Ubuntu-14.04-LTS_i386_static
+        Ubuntu-14.04-LTS_i386_shared
         "
+
+# CONFIGS="Ubuntu-14.04-LTS_amd64_static Ubuntu-14.04-LTS_i386_static"
+# CONFIGS="Ubuntu-14.04-LTS_amd64_shared Ubuntu-14.04-LTS_i386_shared"
+# CONFIGS="AstraLinux-1.4_static AstraLinux-1.3_static AstraLinux-1.2_static"
+# CONFIGS="AstraLinux-1.4_shared AstraLinux-1.3_shared"
+
 
 # Type of current system:
 BUILD="$(LC_ALL=C gcc -v 2>&1 | sed -ne "s|^Target: \(.*\)$|\1|p")"
