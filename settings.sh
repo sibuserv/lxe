df21013d0021badcb12485fd57701b1185295ab0
Boris Pek <tehnick-8@yandex.ru>
2018-03-16 00:53:28 +0300

    Update settings.

--- settings.sh
+++ settings.sh
@@ -30,9 +30,9 @@ JOBS=$(nproc 2>/dev/null || echo 1)
 
 # Default list of packages:
 # LOCAL_PKG_LIST="gcc pkg-config-settings cmake-settings ldd-settings"
-# TODO: hunspell minizip qtkeychain qtwebkit
+# TODO: hunspell minizip miniupnpc qtkeychain qtwebkit gstreamer gst-plugins-base lua
 LOCAL_PKG_LIST="cmake-settings ldd-settings boost aspell libotr tidy-html5
-                qtbase qttools qtmultimedia qca libsignal-protocol-c"
+                libsignal-protocol-c qtbase qttools qtmultimedia qca"
 
 # List of packages which should provide static libraries in environments with
 # shared libraries (see DEFAULT_LIB_TYPE variable in configs):
