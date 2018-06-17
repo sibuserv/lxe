7c73b92052b5e10665cd3d65ad9ac1eaac13c208
Boris Pek <tehnick-8@yandex.ru>
2018-06-18 00:03:23 +0300

    openssl: update PKG_URL and PKG_URL_2.

--- pkg/openssl.sh
+++ pkg/openssl.sh
@@ -9,8 +9,8 @@
     PKG_VERSION=${OPENSSL_VER}
     PKG_SUBDIR=${PKG}-${PKG_VERSION}
     PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
-    PKG_URL="ftp://ftp.openssl.org/source/${PKG_FILE}"
-    PKG_URL_2="ftp://ftp.openssl.org/source/old/${OPENSSL_SUBVER}/${PKG_FILE}"
+    PKG_URL="https://ftp.openssl.org/source/${PKG_FILE}"
+    PKG_URL_2="https://ftp.openssl.org/source/old/${OPENSSL_SUBVER}/${PKG_FILE}"
     PKG_DEPS="gcc zlib libgcrypt"
 
     CheckSourcesAndDependencies
