4e20fa589e1dc933b1070d412f44e0095ec97e3d
Boris Pek <tehnick-8@yandex.ru>
2018-06-29 01:08:10 +0300

    Add new packages: icu, qtwebkit.

---
+++ pkg/qtwebkit.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# This file is part of LXE project. See LICENSE file for licensing information.
+
+(
+    PKG=qtwebkit
+    PKG_VERSION=${QTWEBKIT_VER}
+    PKG_SUBDIR=${PKG}-${PKG_VERSION}
+    PKG_SUBDIR_ORIG=${PKG}-${QTWEBKIT_GIT_VER}
+    PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
+    PKG_URL="https://github.com/qt/qtwebkit/archive/${PKG_VERSION}.tar.gz"
+    PKG_DEPS="gcc cmake-settings icu sqlite qtbase qtmultimedia qtquickcontrols"
+    [ ! -z "${GCC_EXTRA_VER}" ] && PKG_DEPS="${PKG_DEPS} gcc-extra"
+
+    CheckPkgVersion
+    CheckSourcesAndDependencies
+
+    if IsBuildRequired
+    then
+        # Temporary workaround until static build is fixed:
+        if IsStaticPackage
+        then
+            CheckDependencies
+
+            date -R > "${INST_DIR}/${PKG}"
+            echo "[config]   ${CONFIG}"
+            echo "[no-build] ${PKG}"
+
+            exit 0
+        fi
+
+        PrintSystemInfo
+        BeginOfPkgBuild
+        UnpackSources
+        PrepareBuild
+
+        SetBuildFlags "${GCC_EXTRA_VER}"
+        UpdateGCCSymlinks "${GCC_EXTRA_VER}"
+        SetCrossToolchainVariables "${GCC_EXTRA_VER}"
+        SetCrossToolchainPath
+        ConfigureQmakeProject \
+            "${PKG_SRC_DIR}/${PKG_SUBDIR_ORIG}/WebKit.pro"
+
+        BuildPkg -j ${JOBS}
+        InstallPkg install
+
+        CleanPkgBuildDir
+        CleanPkgSrcDir
+
+        UpdateGCCSymlinks
+
+        find "${SYSROOT}/qt5/lib" -type f -name '*.la' -exec rm -f {} \;
+    fi
+)
+
