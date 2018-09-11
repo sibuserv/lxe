d3f607e3eb52211a4bbecdcf2700b2a11591511b
Boris Pek <tehnick-8@yandex.ru>
2018-09-12 00:36:45 +0300

    Add new package: lua.

---
+++ pkg/lua.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#
+# This file is part of LXE project. See LICENSE file for licensing information.
+
+(
+    PKG=lua
+    PKG_VERSION=${LUA_VER}
+    PKG_SUBDIR=${PKG}-${PKG_VERSION}
+    PKG_FILE=${PKG}-${PKG_VERSION}.tar.gz
+    PKG_URL="https://www.lua.org/ftp/${PKG_FILE}"
+    PKG_DEPS="gcc"
+
+    CheckPkgVersion
+    CheckSourcesAndDependencies
+
+    if IsBuildRequired
+    then
+        PrintSystemInfo
+        BeginOfPkgBuild
+        UnpackSources
+        CopySrcAndPrepareBuild
+
+        SetBuildFlags
+        SetCrossToolchainVariables
+        SetCrossToolchainPath
+
+        BuildPkg -j ${JOBS} \
+            INSTALL_TOP="${SYSROOT}/usr" \
+            CC="${CC}" \
+            RANLIB="${RANLIB}" \
+            linux
+        InstallPkg -j 1 \
+            INSTALL_TOP="${SYSROOT}/usr" \
+            install
+
+        DeleteExtraFiles
+        EndOfPkgBuild
+
+        CleanPkgBuildDir
+        CleanPkgSrcDir
+    fi
+)
+
