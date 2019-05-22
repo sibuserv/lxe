e3dc656ae5c2542f010c61d66fd169e36ed85c78
Boris Pek <tehnick-8@yandex.ru>
2019-05-23 02:20:02 +0300

    libgpg-error: update patch.

--- pkg/libgpg-error-patches.sh
+++ pkg/libgpg-error-patches.sh
@@ -11,7 +11,12 @@
         USE_ARCH=i686 || \
         USE_ARCH=${ARCH}
 
-    if IsPkgVersionGreaterOrEqualTo "1.19"
+    if IsPkgVersionGreaterOrEqualTo "1.30"
+    then
+        cd "${PKG_SRC_DIR}/${SUBDIR}/src/syscfg"
+        ln -sf lock-obj-pub.${USE_ARCH}-unknown-linux-gnu.h \
+               lock-obj-pub.linux-gnu.h
+    elif IsPkgVersionGreaterOrEqualTo "1.19"
     then
         cd "${PKG_SRC_DIR}/${SUBDIR}/src/syscfg"
         ln -sf lock-obj-pub.${USE_ARCH}-pc-linux-gnu.h \
