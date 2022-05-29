f06f8377bc52ce966c941568a2290496b7a2bbe7
Boris Pek <tehnick-8@yandex.ru>
2022-05-29 19:30:33 +0300

    Update settings.

--- settings.sh
+++ settings.sh
@@ -1,26 +1,20 @@
 #!/bin/sh
 
+
 # All available configs are in "etc/" subdirectory. Select necessary of them:
-# CONFIGS="
-#         AstraLinux-1.5_static
-#         AstraLinux-1.5_shared
-#         AstraLinux-1.4_static
-#         AstraLinux-1.4_shared
-#         AstraLinux-1.3_static
-#         AstraLinux-1.3_shared
-#         AstraLinux-1.2_static
-#         AstraLinux-1.2_shared
-#         Ubuntu-14.04_amd64_static
-#         Ubuntu-14.04_amd64_shared
-#         Ubuntu-14.04_i386_static
-#         Ubuntu-14.04_i386_shared
-#         "
-
-# CONFIGS="AstraLinux-1.5_static AstraLinux-1.4_static AstraLinux-1.3_static"
-# CONFIGS="AstraLinux-1.5_shared AstraLinux-1.3_shared"
-# CONFIGS="Ubuntu-14.04_amd64_static Ubuntu-14.04_i386_static"
-CONFIGS="Ubuntu-14.04_amd64_shared Ubuntu-14.04_i386_shared"
-
+CONFIGS=""
+CONFIGS="${CONFIGS} Ubuntu-14.04_amd64_shared"
+CONFIGS="${CONFIGS} Ubuntu-14.04_amd64_static"
+CONFIGS="${CONFIGS} Ubuntu-14.04_i386_shared"
+CONFIGS="${CONFIGS} Ubuntu-14.04_i386_static"
+#CONFIGS="${CONFIGS} AstraLinux-1.5_shared"
+#CONFIGS="${CONFIGS} AstraLinux-1.5_static"
+#CONFIGS="${CONFIGS} AstraLinux-1.4_shared"
+#CONFIGS="${CONFIGS} AstraLinux-1.4_static"
+#CONFIGS="${CONFIGS} AstraLinux-1.3_shared"
+#CONFIGS="${CONFIGS} AstraLinux-1.3_static"
+#CONFIGS="${CONFIGS} AstraLinux-1.2_shared"
+#CONFIGS="${CONFIGS} AstraLinux-1.2_static"
 
 # Type of current system:
 BUILD="$(LC_ALL=C gcc -v 2>&1 | sed -ne "s|^Target: \(.*\)$|\1|p")"
